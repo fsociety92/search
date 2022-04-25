fef07f69e3f44e8458a5c6986b0c86bd6666e882
fsociety92 <emil.1emil92@gmail.com>
2022-04-25 11:11:53 +0200

    first commit

---
+++ src/router/search.ts
@@ -0,0 +1,21 @@
+// import { Controller, Injectable, Get } from '@nestjs/common'
+// import { InjectRepository } from '@nestjs/typeorm'
+// import { FilterOperator, Paginate, PaginateQuery, paginate, Paginated } from 'nestjs-paginate'
+// import { Repository, Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+
+
+// @Entity()
+// export class CatEntity {
+//   @PrimaryGeneratedColumn()
+//   id: number
+
+//   @Column('text')
+//   name: string
+
+//   @Column('text')
+//   color: string
+
+//   @Column('int')
+//   age: number
+// }
+
